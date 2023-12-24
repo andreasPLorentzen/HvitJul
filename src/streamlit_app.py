23482cc8038b6a662a87f8fd2c62cd7fdd1c4b29
Andreas Lorentzen <adlr@cowi.com>
2023-12-24 18:54:41 +0100

    First

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -4,7 +4,7 @@ from folium.plugins import Draw
 from streamlit_folium import st_folium
 
 from .utils import polygon_from_point
-import random
+import random, requests
 
 # MAP settings
 INPUT_MAP_CENTER = [60.0, 10.0]
@@ -81,8 +81,36 @@ def draw_trip_in_map():
 
     return data
 
+def get_place_names(query):
+    base_url = "https://api.kartverket.no/stedsnavn/v1/navn"
+    params = {
+        "sok": query,
+        "fuzzy": "true",
+        "utkoordsys": "4258",
+        "treffPerSide": "10",
+        "side": "1"
+    }
+
+    response = requests.get(base_url, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return []
+
 def wrapper_page():
     # st.set_page_config(layout="wide")
+
+    st.title("Place Name Autocomplete")
+
+    place_query = st.text_input("Enter a place name", "")
+    if place_query:
+        place_names = get_place_names(place_query)
+        if 'navn' in place_names:
+            options = [place['navn'] for place in place_names['navn']]
+            st.selectbox("Select a place", options)
+        else:
+            st.warning("No places found for your query.")
+
 
     data = draw_trip_in_map()
     # st.write(data)
