0f97025bb3600d81970097d6229a2572861e3699
Andreas Lorentzen <adlr@cowi.com>
2023-12-24 22:16:17 +0100

    First

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -97,14 +97,14 @@ def get_place_names(query):
     else:
         return []
 
-def get_x_first_place_names(query, x=10) -> list:
+def get_x_first_place_names(query, x=10) -> (list,list):
     api_request = get_place_names(query)
     return_list = []
 
     if api_request == []:
         return []
 
-    st.write(api_request["navn"])
+    # st.write(api_request["navn"])
 
     for index, data in enumerate(api_request["navn"]):
         # if data["navneobjekttype"]
@@ -114,6 +114,25 @@ def get_x_first_place_names(query, x=10) -> list:
             break
     return return_list, api_request
 
+def get_place_name(lat,long):
+    url = f"https://api.kartverket.no/stedsnavn/v1/punkt?nord={lat}&ost={long}&koordsys=4258&radius=500&utkoordsys=4258&treffPerSide=10&side=1"
+    base_url = "https://api.kartverket.no/stedsnavn/v1/navn"
+    # params = {
+    #     "sok": query,
+    #     "fuzzy": "true",
+    #     "utkoordsys": "4258",
+    #     "treffPerSide": "10",
+    #     "side": "1"
+    # }
+
+    response = requests.get(base_url)
+    if response.status_code == 200:
+        st.write(response.json())
+        return response.json()
+    else:
+        return []
+
+
 def placenames_options(querey):
     if len(querey) < 4:
         return [""]
@@ -122,14 +141,14 @@ def placenames_options(querey):
 def wrapper_page():
     # st.set_page_config(layout="wide")
 
-    st.title("Place Name Autocomplete")
+    st.title("Hvor hvit er egentlig jula?")
 
     place_query = st.text_input("Enter a place name", "")
     if place_query:
         # place_names = get_place_names(place_query)
         # st.write(place_names)
         first_10_place_names, place_names = get_x_first_place_names(place_query, 10)
-        st.write(first_10_place_names)
+        # st.write(first_10_place_names)
         if 'navn' in place_names:
             # options = [place['navn'] for place in place_names['navn']]
             options = first_10_place_names
@@ -137,18 +156,19 @@ def wrapper_page():
         else:
             st.warning("No places found for your query.")
 
-    test = "Tynset"
-    options = placenames_options(test)
-    test = st.selectbox("Søk på stedsnavn", placenames_options(test))
-    st.write(test)
+    # test = "Tynset"
+    # options = placenames_options(test)
+    # test = st.selectbox("Søk på stedsnavn", placenames_options(test))
+    # st.write(test)
 
     data = draw_trip_in_map()
     # st.write(data)
     st.write(data["last_clicked"])
 
     if data["last_clicked"] is not None:
-        polygon =polygon_from_point(data["last_clicked"]["lat"], data["last_clicked"]["lng"], 1)
-        st.write(polygon)
+        # polygon =polygon_from_point(data["last_clicked"]["lat"], data["last_clicked"]["lng"], 1)
+        # st.write(polygon)
+        location_name =
 
 
     write_trees({"ost":1})
