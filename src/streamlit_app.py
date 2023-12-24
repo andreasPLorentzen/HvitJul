a1e1603578a58521466b0051e36e9d3b6b808361
Andreas Lorentzen <adlr@cowi.com>
2023-12-24 19:06:59 +0100

    First

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -100,9 +100,11 @@ def get_place_names(query):
 def get_x_first_place_names(query, x) -> list:
     api_request = get_place_names(query)
     return_list = []
-    st.write(api_request)
+
     if api_request == []:
         return []
+
+    st.write(api_request["navn"])
     for index,data in api_request["navn"].items():
         return_list.append(data["skrivemåte"])
 
