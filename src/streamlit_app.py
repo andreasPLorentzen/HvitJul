e339b4b1043e1c415bb9e69aaaa73414cc80c0f1
Andreas Lorentzen <adlr@cowi.com>
2023-12-24 14:36:57 +0100

    First

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -89,8 +89,8 @@ def write_trees(years=dict):
         years_list.append((year, category))
 
     for row in range(0,3):
-        col1,col2,col3,col4,col5 = st.columns(5)
-        cols = [col1,col2,col3,col4,col5]
+        col1,col2,col3,col4,col5,col6,col7,col8,col9,col10 = st.columns(10)
+        cols = [col1,col2,col3,col4,col5,col6,col7,col8,col9,col10]
         for col in cols:
             with col:
                 st.image(f"Graphics/SNOW_{random.randint(0,3)}.png")
