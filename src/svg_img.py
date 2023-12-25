e6bdbdf99fe5b74cd8c2c689bfd6203eb6effb48
Andreas Lorentzen <adlr@cowi.com>
2023-12-25 21:59:48 +0100

    First

--- src/svg_img.py
+++ src/svg_img.py
@@ -27,7 +27,7 @@ class image_generation:
             st.write("SNOW_LEVEL_" + condition)
             st.write("SNOW_LEVEL_" + condition in self.images.keys())
             st.write(self.images["SNOW_LEVEL_" + condition])
-            svg_list.append(self._alter_text_in_image(, year=year,snow_depth=depth))
+            svg_list.append(self._alter_text_in_image(self.images["SNOW_LEVEL_" + condition], year=year,snow_depth=depth))
 
 
         # alter top and info
