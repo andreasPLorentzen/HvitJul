d65ee54806b44d4bcca54222222f4d123309998c
Andreas Lorentzen <adlr@cowi.com>
2023-12-26 03:15:54 +0100

    First

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -267,7 +267,7 @@ def main_page():
     data = st_folium(m, height=INPUT_MAP_HEIGHT, width=INPUT_MAP_WIDTH, key="new", feature_group_to_add=fg)
 
     earliest_year = st.select_slider("Hvor langt tilbake i tid vil du se?",
-                                     options=[y for y in range(1950, this_year).__reversed__()],
+                                     options=[y for y in range(1957, this_year).__reversed__()],
                                      value=this_year - 14)
 
     if data["last_clicked"] is not None:
@@ -368,7 +368,7 @@ def more_info():
     # with st.expander("Les mer om prosjektet", expanded=False):
     st.subheader("Om prosjektet")
     st.markdown("<p>Denne løsningen var et hobbyprosjekt i jula 2023 av Andreas P. Lorentzen og Johannes P. Lorentzen som startet i en diskusjon og endte med implementasjon. Vi håper at du og dere liker løsningen, og at det kanskje hjelper med å løse en diskusjon hos dere også.</p> "
-                "<p>Løsningen benytter NVE sin API for xgeo.no, som gir data om beregnet snødybde for et gitt punkt. Dette gjorde det enkelt for oss, men er ikke like presist som å bruke målinger fra målestasjoner. Vi bruker Kartverket sin stedsnavn-API for å hente stedsnavn.</p>"
+                "<p>Løsningen benytter NVE sin API for xgeo.no, som gir data om beregnet snødybde for et gitt punkt. Dette gjorde det enkelt for oss, men er ikke like presist som å bruke målinger fra målestasjoner. API-et leverer data tilbake til 1957, som derfor er satt som tidligste år. Vi bruker Kartverket sin stedsnavn-API for å hente stedsnavn.</p>"
                 "<p>Alt er implementert i Python ved bruk av pakken streamlit. Bruker du Python, så anbefaler vi å prøve den ut. Det er derimot noen svakheter med systemet. Spesielt en vi ikke har klart å løse med en treg markør i kartet. Hvis du vil titte på kildekoden ligger den tilgjengelig på GitHub.</p>"
                 "<p>Hvis du ønsker å ta kontakt, gjør det gjerne gjennom LinkedIn.</p>"
                 "<p>Andreas: <a href='https://www.linkedin.com/in/andreas-p-lorentzen/'>https://www.linkedin.com/in/andreas-p-lorentzen</a></p>"
