9fb00ac3537e02cda7502e03f14256a8b6d03cd5
Andreas Lorentzen <adlr@cowi.com>
2023-12-26 03:38:45 +0100

    First

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -247,7 +247,6 @@ def main_page():
 
     st.markdown(
         "<p>Etter vi så en grafikk som viste om det var en hvit jul i New York, begynte vi å diskutere på hvor ofte det egentlig var en hvit jul i Oslo? eller Bergen? Vi ble ikke helt enige og siden vi strengt talt ikke hadde bedre ting å gjøre mellom turer på ski og julemat, så lagde vi denne websiden som lar deg velge et sted i Norge og få svaret selv.</p>"
-        "<p>Siden vi strengt talt hadde bedre ting å gjøre, så lagde vi denne websiden som lar deg velge et sted i Norge og få svaret selv.</p>"
         "<p>Når du trykker i kartet, starter applikasjonen å hente data om snøforhold fra NVE og sjekker hvordan var snøforholdene akkurat der. Du kan selv velge hvor langt tilbake i tid du ønsker å se ved å bruke skyveknappen nedenfor, men vi anbefaler å starte med kun 14 år, da det tar litt tid å hente inn data.</p>", unsafe_allow_html=True)
     st.subheader("Trykk i kartet for å se hvor mye snø det har vært i jula")
 
